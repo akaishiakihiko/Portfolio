805d84ce8f0e75adcc434a1c5ce84c742c602cc6
akaishiakihiko <o9028743602@gmail.com>
2022-08-29 10:53:43 +0900

    新規登録

---
+++ js/function.js
@@ -0,0 +1,137 @@
+//オープニング//
+//logoの表示
+$(window).on('load', function () {
+  $("#splash").delay(2000).fadeOut('slow');//ローディング画面を1.5秒（1500ms）待機してからフェードアウト
+  $("#splash_logo").delay(1200).fadeOut('slow');//ロゴを1.2秒（1200ms）待機してからフェードアウト
+});
+
+
+
+
+
+//ページ内リンク//
+$('#page-link a[href*="#"]').click(function () {//全てのページ内リンクに適用させたい場合はa[href*="#"]のみでもOK
+  var elmHash = $(this).attr('href'); //ページ内リンクのHTMLタグhrefから、リンクされているエリアidの値を取得
+  var pos = $(elmHash).offset().top;	//idの上部の距離を取得
+  $('body,html').animate({ scrollTop: pos }, 500); //取得した位置にスクロール。500の数値が大きくなるほどゆっくりスクロール
+  return false;
+});
+
+
+
+//ループスライダー順序よく//
+$('.slider').slick({
+  arrows: false,//左右の矢印はなし
+  autoplay: true,//自動的に動き出すか。初期値はfalse。
+  autoplaySpeed: 0,//自動的に動き出す待ち時間。初期値は3000ですが今回の見せ方では0
+  speed: 8000,//スライドのスピード。初期値は300。
+  infinite: true,//スライドをループさせるかどうか。初期値はtrue。
+  pauseOnHover: false,//オンマウスでスライドを一時停止させるかどうか。初期値はtrue。
+  pauseOnFocus: false,//フォーカスした際にスライドを一時停止させるかどうか。初期値はtrue。
+  cssEase: 'linear',//動き方。初期値はeaseですが、スムースな動きで見せたいのでlinear
+  slidesToShow: 2,//スライドを画面に4枚見せる
+  slidesToScroll: 1,//1回のスライドで動かす要素数
+  responsive: [
+    {
+      breakpoint: 800,//モニターの横幅が769px以下の見せ方
+      settings: {
+        slidesToShow: 2,//スライドを画面に2枚見せる
+      }
+    },
+    {
+      breakpoint: 500,//モニターの横幅が426px以下の見せ方
+      settings: {
+        slidesToShow: 1.5,//スライドを画面に1.5枚見せる
+      }
+    }
+  ]
+});
+
+
+//ループスライダー順序逆//
+$('.slider2').slick({
+  arrows: false,//左右の矢印はなし
+  autoplay: true,//自動的に動き出すか。初期値はfalse。
+  autoplaySpeed: 0,//自動的に動き出す待ち時間。初期値は3000ですが今回の見せ方では0
+  speed: 8000,//スライドのスピード。初期値は300。
+  infinite: true,//スライドをループさせるかどうか。初期値はtrue。
+  pauseOnHover: false,//オンマウスでスライドを一時停止させるかどうか。初期値はtrue。
+  pauseOnFocus: false,//フォーカスした際にスライドを一時停止させるかどうか。初期値はtrue。
+  cssEase: 'linear',//動き方。初期値はeaseですが、スムースな動きで見せたいのでlinear
+  slidesToShow: 2,//スライドを画面に4枚見せる
+  slidesToScroll: 1,//1回のスライドで動かす要素数
+  rtl: true, // スライダーを左から右に流す（逆向き）
+  responsive: [
+    {
+      breakpoint: 800,//モニターの横幅が769px以下の見せ方
+      settings: {
+        slidesToShow: 2,//スライドを画面に2枚見せる
+      }
+    },
+    {
+      breakpoint: 500,//モニターの横幅が426px以下の見せ方
+      settings: {
+        slidesToShow: 1.5,//スライドを画面に1.5枚見せる
+      }
+    }
+  ]
+});
+
+
+
+
+// 動きのきっかけとなるアニメーションの名前を定義
+function fadeAnime() {
+
+  //ふわっと動くきっかけのクラス名と動きのクラス名の設定
+  $('.fadeUpTrigger').each(function () { //fadeInUpTriggerというクラス名が
+    var elemPos = $(this).offset().top - 50; //要素より、50px上の
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll >= elemPos - windowHeight) {
+      $(this).addClass('fadeUp');
+      // 画面内に入ったらfadeInというクラス名を追記
+    } else {
+      $(this).removeClass('fadeUp');
+      // 画面外に出たらfadeInというクラス名を外す
+    }
+  });
+
+  //関数でまとめることでこの後に違う動きを追加することが出来ます
+  $('.fadeDownTrigger').each(function () { //fadeInDownTriggerというクラス名が
+    var elemPos = $(this).offset().top - 50; //要素より、50px上の
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll >= elemPos - windowHeight) {
+      $(this).addClass('fadeDown');
+      // 画面内に入ったらfadeDownというクラス名を追記
+    } else {
+      $(this).removeClass('fadeDown');
+      // 画面外に出たらfadeDownというクラス名を外す
+    }
+  });
+
+
+}//ここまでふわっと動くきっかけのクラス名と動きのクラス名の設定
+
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function () {
+  fadeAnime();/* アニメーション用の関数を呼ぶ*/
+});// ここまで画面をスクロールをしたら動かしたい場合の記述
+
+// 画面が読み込まれたらすぐに動かしたい場合の記述
+$(window).on('load', function () {
+  fadeAnime();/* アニメーション用の関数を呼ぶ*/
+});// ここまで画面が読み込まれたらすぐに動かしたい場合の記述
+
+
+
+
+
+// #page-topをクリックした際の設定
+$('.footer-pagetop').click(function () {
+  $('body,html').animate({
+    scrollTop: 0//ページトップまでスクロール
+  }, 500);//ページトップスクロールの速さ。数字が大きいほど遅くなる
+  return false;//リンク自体の無効化
+});
